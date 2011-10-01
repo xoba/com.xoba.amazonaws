c04b6bbd11bdc9acf934fd56f9188cb68c3d9e8d
mike andrews <mra@xoba.com>
2011-10-01 10:13:54 -0400

    eclipse wrapper for aws sdk

---
+++ src/com/xoba/amazonaws/AWSUtils.java
@@ -0,0 +1,41 @@
+package com.xoba.amazonaws;
+
+import java.util.Iterator;
+import java.util.List;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+public class AWSUtils {
+
+	public static interface IBucketListener {
+
+		public boolean add(S3ObjectSummary s);
+
+		public void done();
+
+	}
+
+	public static void scanObjectsInBucket(AmazonS3 s3, String bucket, IBucketListener listener) throws Exception {
+		ObjectListing list = s3.listObjects(bucket);
+		boolean done = false;
+		while (!done) {
+			List<S3ObjectSummary> ss = list.getObjectSummaries();
+			Iterator<S3ObjectSummary> it = ss.iterator();
+			while (it.hasNext() && !done) {
+				S3ObjectSummary s = it.next();
+				if (!listener.add(s)) {
+					done = true;
+				}
+			}
+			if (list.isTruncated()) {
+				list = s3.listNextBatchOfObjects(list);
+			} else {
+				done = true;
+			}
+		}
+		listener.done();
+	}
+
+}
